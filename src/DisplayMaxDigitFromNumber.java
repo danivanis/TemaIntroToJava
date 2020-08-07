b747990c352282af56fc6f1061ae135a88e9d722
Danut Ivanis <dan.ivanis@pm.me>
2020-08-07 03:31:41 +0300

    The 3rd part of the homework is done :)

--- src/DisplayMaxDigitFromNumber.java
+++ src/DisplayMaxDigitFromNumber.java
@@ -1,9 +1,18 @@
-import java.security.DigestInputStream;
-
 public class DisplayMaxDigitFromNumber {
     public static void main(String[] args) {
 
-        int n = 68249;
-        System.out.println("The max digit from the number is " + n % 10);
+        int n = 6824925;
+        int maxDigit = 0;
+
+        while (n != 0){
+
+            int remainder = n % 10;
+
+            maxDigit = Math.max(remainder, maxDigit);
+
+            n = n/10;
+        }
+
+        System.out.println("The max digit from the number is " + maxDigit);
     }
 }
